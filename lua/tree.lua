200be9d4f2969e83ab306348cf3f810ecf81ad1c
Anders <dom.galbavy@gmail.com>
2023-01-03 18:18:15 +0100

    changed how git icons are displayed

--- lua/tree.lua
+++ lua/tree.lua
@@ -2,4 +2,13 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup {
+	open_on_setup = true,
+	renderer = {
+		group_empty = true,
+		highlight_git = true,
+		icons = {
+			git_placement = "signcolumn"
+		}
+	}
+}
