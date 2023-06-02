83cc54169b8fbe10f4e8a1436292b36b8ae74e1c
Anders <dom.galbavy@gmail.com>
2023-06-03 01:52:56 +0200

    added coc.lua

---
+++ lua/coc.lua
@@ -0,0 +1,13 @@
+
+-- the code between the brackets is interpreted as vimscript code
+
+vim.cmd([[
+
+" confirms selection in completion list
+inoremap <expr> <cr> coc#pum#visible() ? coc#pum#confirm() : "\<CR>"
+
+" navigate completion list with tab
+inoremap <expr> <Tab> coc#pum#visible() ? coc#pum#next(1) : "\<Tab>"
+inoremap <expr> <S-Tab> coc#pum#visible() ? coc#pum#prev(1) : "\<S-Tab>"
+
+]])
