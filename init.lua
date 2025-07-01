497d8fd6fefa424e3da31bad244523929494c3e0
GalbavyD <dominik@galbavy.net>
2025-07-01 03:17:00 +0200

    disabled most settings and reactivated some

--- init.lua
+++ init.lua
@@ -3,41 +3,41 @@
 require 'paq' {
 	-- paq itself
 	'savq/paq-nvim',
-	'nvim-tree/nvim-web-devicons',		-- required by file explorer
-	'lewis6991/gitsigns.nvim',				-- adds git integration
+	'nvim-tree/nvim-web-devicons',    -- required by file explorer for file icons
+	-- 'lewis6991/gitsigns.nvim',        -- adds git integration
 
 	-- color schemes and themes
-	'EdenEast/nightfox.nvim',					-- color schemes
+	'EdenEast/nightfox.nvim',         -- color schemes, currently unused
 
 	-- layout stuff
-	'nvim-tree/nvim-tree.lua',				-- file explorer
-	'freddiehaddad/feline.nvim',			-- status line
+	'nvim-tree/nvim-tree.lua',        -- file explorer
+	-- 'freddiehaddad/feline.nvim',      -- status line
 
 	-- Mason and stuff to set it up
 	-- A plugin for LSP and debuggers
-	{ 'williamboman/mason.nvim', run = ':MasonUpdate' },
-	'williamboman/mason-lspconfig.nvim',
-	'neovim/nvim-lspconfig',
-	--'mfussenegger/nvim-dap',
-	--'rcarriga/nvim-dap-ui',
+	-- { 'williamboman/mason.nvim', run = ':MasonUpdate' },
+	-- 'williamboman/mason-lspconfig.nvim',
+	-- 'neovim/nvim-lspconfig',
+	-- 'mfussenegger/nvim-dap',
+	-- 'rcarriga/nvim-dap-ui',
 
 	-- syntax highlighting, as LSP highlighting is not ideal
-	'leafOfTree/vim-svelte-plugin',		
+	-- 'leafOfTree/vim-svelte-plugin',
 }
 
 
 require('settings')
 require('keybinds')
 
-require('gitsigns-config')
+-- require('gitsigns-config')
 
 require('colors')
 
 require('tree')
-require('statusline')
+-- require('statusline')
 
-require('lsp-config')
+-- require('lsp-config')
 
 
-vim.g.vim_svelte_plugin_use_typescript = true
+-- vim.g.vim_svelte_plugin_use_typescript = true
 
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,18 +1,20 @@
 -- shorthand for all options
 local o = vim.opt
 
-o.breakindent = true
-o.number = true
-o.relativenumber = true
-o.colorcolumn = '80'
-o.cursorline = true
-o.tabstop = 2
-o.softtabstop = 2
-o.shiftwidth = 2
-o.syntax = 'on'
+
+o.breakindent = true         -- wrapped line repeats indent
+o.number = true              -- print the line number in front of each line
+o.relativenumber = true      -- show relative line number in front of each line
+o.colorcolumn = '80'         -- columns to highlight
+o.cursorline = true          -- highlight the screen line of the cursor
+o.tabstop = 2                -- number of spaces that <Tab> in file uses
+o.softtabstop = 2            -- number of spaces that <Tab> uses while editing
+o.shiftwidth = 2             -- number of spaces to use for (auto)indent step
+o.list = true                -- show <Tab> and <EOL>
+o.syntax = 'on'              -- syntax to be loaded for current buffer
 
 -- required by at least the file explorer
-vim.opt.termguicolors = true
+-- o.termguicolors = true
 
 -- prevents some weird split opening on <C-Space> Completion
-vim.opt.completeopt = 'menu'
+-- o.completeopt = 'menu'
